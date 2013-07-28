f411a7688475f590478d8eb6bc0b3604c652c8d8
Sebastian <sebas.goldberg@gmail.com>
2013-07-27 19:01:55 -0700

    Corrección de template de insersion SQL

--- templates/temp/db/load_default_data.sql
+++ templates/temp/db/load_default_data.sql
@@ -4,5 +4,5 @@ INSERT INTO `alias` (`source`, `destination`, `enabled`) VALUES ('@localhost', '
 INSERT INTO `alias` (`source`, `destination`, `enabled`) VALUES ('@localhost.localdomain', '@localhost', 1);
 INSERT INTO `domain` (`domain`, `transport`, `enabled`) VALUES ('localhost', 'virtual:', 1);
 INSERT INTO `domain` (`domain`, `transport`, `enabled`) VALUES ('localhost.localdomain', 'virtual:', 1);
-INSERT INTO `domain` (`domain`, `transport`, `enabled`) VALUES ('dev.iamsoft.com.ar', 'virtual:', 1);
+INSERT INTO `domain` (`domain`, `transport`, `enabled`) VALUES ('{{server}}', 'virtual:', 1);
 INSERT INTO `user` (`email`, `password`, `name`, `quota`, `enabled`) VALUES ('{{user}}@{{server}}', ENCRYPT('{{password}}'), '{{name}}', NULL, 1);
